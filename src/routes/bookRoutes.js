67b4dfada9121abe2029e98f9f275ce52b4ea673
Adolfo <adolfo.gomes@al.infnet.edu.br>
2025-08-19 10:34:12 -0300

    feat: criação das rotas dos livros

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const bookController = require('../controllers/bookControler');
+const bookController = require('../controllers/bookController');
 
 router.get('/', bookController.getBooks);
 
 router.get('/:id', bookController.getBookByTitle);
+
+router.post('/', bookController.createBook);
 
 router.put('/:id', bookController.updateBook)
 
