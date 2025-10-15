659c9b4c0d01ab42ada0b674eb4313d731a27f74
adolfo.gomes@al.infnet.edu.br <adolfo.gomes@al.infnet.edu.br>
2025-10-15 11:37:17 -0300

    test

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const path = require('path');
 // testando 2
 const logger = winston.createLogger({
-  level: 'info', // nível mínimo que será logado (debug < info < warn < error)
+  level: 'info', // nível mínimo que será logado (debug < info < warn < error) MAOEI
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.printf(({ timestamp, level, message }) => {
