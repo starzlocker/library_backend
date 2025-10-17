ee3526df8aca0eadbefe8f207217d2a06b5a98cc
adolfo.gomes@al.infnet.edu.br <adolfo.gomes@al.infnet.edu.br>
2025-10-17 16:35:37 -0300

    feat: corrigindo rotas do crud

--- src/models/Author.js
+++ src/models/Author.js
@@ -1,6 +1,9 @@
 class AuthorModel {
 	constructor(data) {
+		this.id = data.id;
 		this.name = data.name;
+		this.createdAt = data.created_at;
+		this.updatedAt = data.updated_at;
 	}
 }
 
--- src/models/Genre.js
+++ src/models/Genre.js
@@ -1,6 +1,9 @@
 class GenreModel {
 	constructor(data) {
+		this.id = data.id;
 		this.name = data.name;
+		this.createdAt = data.created_at || null;
+		this.updatedAt = data.updated_at || null;
 	}
 }
 
