f34d0a785ad86f1f14ca2aca14ca405fff150887
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-23 02:48:42 -0300

    feat: currency helpers

--- src/utils/mappers.ts
+++ src/utils/mappers.ts
@@ -4,6 +4,7 @@ import type { DBGenreDTO } from '../DTOs/Genre/DBGenreDTO.js';
 import type { Author } from '../models/Author.js';
 import { Book } from '../models/Book.js';
 import type { Genre } from '../models/Genre.js';
+import * as Currency from './currency.js';
 
 export const DBBookDTOasBook = (data: DBBookDTO) => {
   return new Book({
@@ -15,7 +16,7 @@ export const DBBookDTOasBook = (data: DBBookDTO) => {
     coverUrl: data.cover_url,
     description: data.description,
     stock: data.stock,
-    price: data.price,
+    price: data.price ? Currency.fromString(data.price) : data.price,
     createdAt: data.created_at,
     updatedAt: data.updated_at,
   });
