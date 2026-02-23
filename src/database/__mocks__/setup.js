9cffebc488ff02825d6321f1e6ad6c3b2ae3a357
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-23 01:10:18 -0300

    refactor: removes old code

--- src/database/__mocks__/setup.js
+++
@@ -1,7 +0,0 @@
-// Mock do setup.js para testes
-const dbConnect = jest.fn().mockResolvedValue({
-    query: jest.fn(),
-    release: jest.fn()
-});
-
-module.exports = { dbConnect };
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
 import cors from 'cors';
 import path from 'node:path';
-import dotenv from 'dotenv';
-// import jwt from 'jsonwebtoken';
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+import { env } from 'node:process';
 import express from 'express';
 
-const PORT = process.env.PORT;
-const URL = process.env.CLIENT_URL;
+const PORT = env.PORT;
+const URL = env.CLIENT_URL;
 
 const router = express.Router();
 
 const bookRoutes = require('./routes/bookRoutes');
-const authRoutes = require('./routes/authRoutes');
-
-router.use('/auth', authRoutes);
 
 router.use('/books', bookRoutes);
 
@@ -26,7 +21,6 @@ app.use(cors({
 
 app.use('/api', router)
 
-
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em ${URL}:${PORT}`)
+    console.log(`Server running in ${URL}:${PORT}`)
 })
