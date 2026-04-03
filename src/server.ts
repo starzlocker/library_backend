68a70c584ee293e224b91a2b2a31b7bdf244a7ab
adolfogomes <Adolfo.Gomes@ibm.com>
2026-04-03 12:36:24 -0300

    fix: app using routes before cors

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,25 @@
 import cors from 'cors';
 import { env } from 'node:process';
 import express from 'express';
-import bookRoutes from './routes/bookRoutes.js'
+import bookRoutes from './routes/bookRoutes.js';
 import { logger } from './config/logger.js';
+import { authRoutes } from './routes/authRoutes.js';
 
 const PORT = env.PORT;
 const FRONTEND = env.CLIENT_URL;
 
-const router = express.Router();
-
-router.use('/books', bookRoutes);
-
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-	origin: FRONTEND
-}));
-
-app.use('/api', router)
-
+app.use(
+  cors({
+    origin: FRONTEND,
+  }),
+);
+app.use('/books', bookRoutes);
+app.use('/auth', authRoutes);
 app.listen(PORT, () => {
-    logger.info(`Server listening on port ${PORT}`)
-})
+  logger.info(`Server listening on port ${PORT}`);
+  logger.info('frontend on ' + FRONTEND);
+  console.log('teste');
+});
