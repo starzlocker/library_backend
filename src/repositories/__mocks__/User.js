55e9a2f213e91c90999501d05e24644ee8ea9032
adolfogomes <Adolfo.Gomes@ibm.com>
2026-04-03 12:36:46 -0300

    refactor: unused files removal

--- src/repositories/__mocks__/User.js
+++
@@ -1,9 +0,0 @@
-// Mock do User repository para testes
-const User = {
-    createUser: jest.fn(),
-    getUserByEmail: jest.fn(),
-    deleteUser: jest.fn(),
-    updateUser: jest.fn()
-};
-
-module.exports = { User };
